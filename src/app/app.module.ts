2ea85c189bff88f3cc02ba469e4bfc0d08a93747
Your Namedexter-site <https://github.com/guga22/dexter-site.git>
2017-03-15 16:04:41 -0300

    editando a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SobreComponent } from './sobre/sobre.component';
 import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, HeaderComponent, FooterComponent ],
+  declarations: [ AppComponent, HeaderComponent , SobreComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
